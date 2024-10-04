8186eb8c8ae1d2c32bddd3266407086229ae68a6
pawaffle <pdegterev@gmail.com>
2024-10-04 15:47:04 +0300

    backend disabled

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const app = require ('./app');
-
-const port = process.env.PORT || 4000;
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
